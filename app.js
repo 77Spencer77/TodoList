9be0d5c62a024cff3667333559c79cdbc4e042a1
Abhranil <deyabhranil1033@gmail.com>
2022-03-08 13:57:36 -0800

    completed single page todolist

--- app.js
+++ app.js
@@ -13,24 +13,8 @@ mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true
 const itemsSchema = {
   name: String,
 };
+
 const Item = mongoose.model("item", itemsSchema);
-
-const item1 = new Item({
-  name: "first task",
-
-});
-const item2 = new Item({
-  name: "second task",
-
-});
-const item3 = new Item({
-  name: "third task",
-
-});
-const defaults = [item1, item2, item3];
-
-
-
 
 app.get("/", function (req, res) {
   var date = new Date();
@@ -42,31 +26,37 @@ app.get("/", function (req, res) {
   var today = date.toLocaleDateString("en-US", options);
 
   Item.find({}, function (err, found) {
-    if (found.length === 0) {
-      Item.insertMany(defaults, function (e) {
-        if (e) {
-          console.log(e);
-        }
-        else {
-          console.log("added succesfully");
-        }
-      });
-      res.redirect("/");
-    }
-    else {
       res.render("list", { today: today, items: found });
-    }
-
   });
 
   //   res.send("adasdasd");
 });
 app.post("/", function (req, res) {
   var task = req.body.task;
-  items.push(task);
-  res.redirect("/");
+  const item = new Item({
+    name:task,
+  });
+  item.save();
+  res.redirect("/"); 
 });
 
-app.listen(8000, function () {
+app.post("/delete",function(req,res){
+  const checked_item=req.body.checkbox;
+  console.log(checked_item);
+  Item.findByIdAndRemove(checked_item,function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.redirect("/");
+      console.log("deletion successfull");
+    }
+
+  });
+
+
+});
+
+app.listen(2000, function () {
   console.log("server is running");
 });
